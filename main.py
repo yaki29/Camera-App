3c46b1f24d302fd56e6a3b6b2f3e4236e7b5d240
yaki29 <yashjain.lnm@gmail.com>
2016-12-06 22:16:04 +0530

    code update

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.app import App 
 from kivy.uix.screenmanager import Screen, ScreenManager , FadeTransition
-from kivy.uix.camera import Camera
+#from kivy.uix.camera import Camera
+from plyer import camera
+from kivy.core.window import Window
 
 Builder.load_string(
 '''
@@ -37,19 +39,29 @@ Builder.load_string(
 		orientation: "vertical"
 		Camera:
 			id: 'camera'
-			resolution: (650, 480)
+			resolution: (1024, 1024)
+
 		Button:
 			text: "Menu"
 			size_hint: None,None
 			width: root.width*0.5
 			height: '48dp'
+			background_color: 0, 0, 0, 1
 			on_press: root.manager.current = "start"
 
 		Button:
-			text: "Front"
-			size_hint: None,None
+			text: "Capture photo"
+			size_hint_y: None
 			width: root.width*0.5
 			height: '48dp'
+			background_color: 0, 0, 0, 1
+			on_press: root.click_pic()
+		# Label: 
+		# 	text: "1"
+		# 	size_hint_y: None
+		# 	width: root.width*0.5
+		# 	height: '48dp'
+
 
 
 ''')
@@ -57,7 +69,13 @@ Builder.load_string(
 class StartScreen(Screen):
 	pass
 class CameraScreen(Screen):
-	pass
+	
+	# def changei(x):
+	# 	return str(int(x) + 1)
+	
+	def click_pic(self):
+		Window.screenshot(name="example.jpeg")
+
 
 sm = ScreenManager(transition=FadeTransition())
 sm.add_widget(StartScreen(name="start"))
