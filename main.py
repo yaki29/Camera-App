339a8d500e3a47f60c6c8a666025e86f6ebaf377
yaki29 <yashjain.lnm@gmail.com>
2016-12-25 02:01:49 +0530

    Audio file added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.pagelayout import PageLayout 
 from kivy.uix.gridlayout import GridLayout 
 from kivy.properties import ObjectProperty
-
+from kivy.core.image import Image
 from kivy.uix.button import Button 
 from kivy.uix.label import Label 
 from kivy.lang import Builder
@@ -14,6 +14,7 @@ from kivy.uix.screenmanager import Screen, ScreenManager , FadeTransition
 #from kivy.uix.camera import Camera
 from plyer.facades import camera
 from kivy.core.window import Window
+from kivy.core.audio import SoundLoader
 
 Builder.load_string(
 '''
@@ -33,6 +34,7 @@ Builder.load_string(
 					size: self.size
 		Button: 
 			# text: "Button 2"
+			on_press: root.manager.current = "audio"
 			background_color: (0,1,0,0.5)
 			BoxLayout:
 				pos: self.parent.pos
@@ -44,6 +46,7 @@ Builder.load_string(
 		Button:
 			# text: "Button 3"
 			background_color: (0,1,0,0.5)
+			on_press: root.manager.current = "gallery"
 			BoxLayout:
 				pos: self.parent.pos
 				size: self.parent.size
@@ -61,10 +64,7 @@ Builder.load_string(
 		Camera:
 			id: camera
 			resolution: (1024, 1024)
-			size_hint: None, None
-			# height: self.texture_size[0]
-			# width: self.texture_size[1]
-			size: self.size
+
 		Button:
 			size_hint: None,None
 			width: '80dp'
@@ -79,37 +79,51 @@ Builder.load_string(
 					source: 'back.png'
 					pos: self.pos
 					size: self.size
-		AnchorLayout:
-			anchor_x: 'center'
-			anchor_y: 'bottom'
-			Button:
-				# text: "Capture photo"
-				size_hint: None, None
-				width: root.width*0.15
-				height: '65dp'
-				background_color: 255, 255, 255, 0.3
-				on_press: root.click_pic()
-				
-				pos: self.parent.x*0.25, self.parent.y
-				BoxLayout:
-					pos: self.parent.pos
-					size: self.parent.size
-					Image: 
-						source: 'capture.png'
-						pos: self.pos
-						size: self.size
-		# Label: 
-		# 	text: "1"
-		# 	size_hint_y: None
-		# 	width: root.width*0.5
-		# 	height: '48dp'
+	
+		Button:
+			# text: "Capture photo"
+			size_hint: None, None
+			width: '80dp'
+			height: '80dp'
+			background_color: 255, 255, 255, 0.3
+			on_press: root.click_pic()
+			# pos_hint: 0.25	
+			# pos: self.parent.x*0.25, self.parent.y
+			BoxLayout:
+				pos: self.parent.pos
+				size: self.parent.size
+				Image: 
+					source: 'capture.png'
+					pos: self.pos
+					size: self.size
+		
+<GalleryScreen>:
+	name: "gallery"
+	BoxLayout:
+		id: gallery_layout
+		Button: 
+			on_press: root.AddImage()	
 
-
-
+<AudioScreen>:
+	GridLayout:
+		cols: 2
+		Button:
+			text: "hello"
+			on_press: root.playmusic()
 ''')
 camera = ObjectProperty(None)
+gallery_layout = ObjectProperty(None)
 class StartScreen(Screen):
 	pass
+class AudioScreen(Screen):
+	def playmusic(self):
+		fname = 'example.wav'
+		sound = SoundLoader.load(fname)
+		# if sound is not None:
+			# print "played"
+			# sound.play()
+		# else:
+		# 	print "Doesn't exist"
 class CameraScreen(Screen):
 	
 	# def changei(x):
@@ -121,11 +135,21 @@ class CameraScreen(Screen):
 		camera.export_to_png("IMG_" + timestr)
 		print "Captured"
 		print time.strftime("%d%m%Y_%H%M%S")
+class GalleryScreen(Screen):
+	# def __init__(self):
+	# 	self.AddImage()
+	def AddImage(self):
+		gallery_layout = self.ids['gallery_layout']
+		# for i in range(0,11):
+		# 	im = Image(source='gallery.png')
+		# 	gallery_layout.add_widget(im)
+
 
 sm = ScreenManager(transition=FadeTransition())
 sm.add_widget(StartScreen(name="start"))
 sm.add_widget(CameraScreen(name="camera"))
-
+sm.add_widget(GalleryScreen(name="gallery"))
+sm.add_widget(AudioScreen(name="audio"))
 class CameraApp(App):
 	sm.current = "start"
 	def build(self):
